785778d06f2da161d10069ebd929c9e28d0ebda5
Haddadson <gabriel.haddad15@gmail.com>
2017-05-20 23:08:52 -0300

    Implements exceptions and UserDAOImpl

--- JQuestPersistenceRAM/src/br/cefetmg/jquest/model/dao/UserDAOImpl.java
+++ JQuestPersistenceRAM/src/br/cefetmg/jquest/model/dao/UserDAOImpl.java
@@ -6,7 +6,10 @@
 package br.cefetmg.jquest.model.dao;
 
 import br.cefetmg.jquest.model.domain.User;
+import br.cefetmg.jquest.model.exception.PersistenceException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -17,8 +20,10 @@ public class UserDAOImpl implements UserDAO {
     
     private static UserDAOImpl userDAO = null;
     private static HashMap<Long, User> userDB = new HashMap<Long, User>();
-    
-    private UserDAOImpl() {};
+    private static long userCount = 0;
+    private UserDAOImpl(){
+        
+    }
     
     public static UserDAOImpl getInstance() {
         if (userDAO == null) {
@@ -28,28 +33,65 @@ public class UserDAOImpl implements UserDAO {
     }
     
     @Override
-    synchronized public void insert(User user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    synchronized public void insert(User user) throws PersistenceException {
+        if (user == null) {
+            throw new PersistenceException("User cannot be null");
+        }
+        Long userId = user.getId();
+              
+        if (userId != null && userDB.containsKey(userId)) {
+            throw new PersistenceException("Duplicate key");
+        }
+        userId = ++userCount;
+        user.setId(userId);
+        userDB.put(userId, user);
     }
 
     @Override
-    synchronized public void update(User user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    synchronized public void update(User user) throws PersistenceException {
+        if (user == null) {
+            throw new PersistenceException("User cannot be null");
+        }
+        Long userId = user.getId();
+        if (userId == null ) {
+            throw new PersistenceException("Entity Id cannot be null");
+        }
+        if (!userDB.containsKey(userId)) {
+            throw new PersistenceException("User with id " + user.getId() + " is not persisted");
+        }
+        userDB.replace(userId, user);
     }
 
     @Override
-    synchronized public User remove(Long userId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    synchronized public User remove(Long userId) throws PersistenceException{
+       if (userId == null) {
+            throw new PersistenceException("User ID cannot be null");
+        }
+        if (!userDB.containsKey(userId)){
+            throw new PersistenceException("User with id " + userId + " is not persisted");
+        }
+        return userDB.remove(userId);
     }
 
     @Override
-    synchronized public User getUserById(Long userId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    synchronized public User getUserById(Long userId) throws PersistenceException{
+        if (userId == null) {
+            throw new PersistenceException("User ID cannot be null");
+        }
+        if (!userDB.containsKey(userId)){
+            throw new PersistenceException("User with id " + userId + " is not persisted");
+        }
+        return userDB.get(userId);
     }
 
     @Override
     public List<User> listAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<User> userList = new ArrayList<>();
+        Iterator<User> it = userDB.values().iterator();
+        while (it.hasNext()) {
+            userList.add(it.next());
+        }
+        return userList;
     }
     
 }

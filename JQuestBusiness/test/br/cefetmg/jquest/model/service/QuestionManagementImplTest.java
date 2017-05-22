9dbe00bc8d8e9c46a6de9224f7892e38691dd36d
Joao Pedro Renan <jota.renan@gmail.com>
2017-05-22 18:54:31 -0300

    Sets templates for tests

---
+++ JQuestBusiness/test/br/cefetmg/jquest/model/service/QuestionManagementImplTest.java
@@ -0,0 +1,27 @@
+package br.cefetmg.jquest.model.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author João Pedro Renan
+ */
+public class QuestionManagementImplTest {
+    
+}

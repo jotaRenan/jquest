90d9d75f9058d26c8b060e860c4931d68048d4e4
Joao Pedro Renan <jota.renan@gmail.com>
2017-05-23 19:38:02 -0300

    Adds DAO, mngr and entity for MultipleChoiceAnswerChoice

---
+++ JQuestCore/src/br/cefetmg/jquest/model/domain/MultipleChoiceAnswerChoice.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.jquest.model.domain;
+
+/**
+ *
+ * @author João Pedro Renan
+ */
+public class MultipleChoiceAnswerChoice {
+    
+    private Long userId;
+    private Long useSeq;
+    private Long questionId;
+    private Long userAnswerSeq;
+    private short optionSeq;
+
+    public MultipleChoiceAnswerChoice() {
+    }
+
+    public MultipleChoiceAnswerChoice(Long userId, Long useSeq, Long questionId, Long userAnswerSeq, short optionSeq) {
+        this.userId = userId;
+        this.useSeq = useSeq;
+        this.questionId = questionId;
+        this.userAnswerSeq = userAnswerSeq;
+        this.optionSeq = optionSeq;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getUseSeq() {
+        return useSeq;
+    }
+
+    public void setUseSeq(Long useSeq) {
+        this.useSeq = useSeq;
+    }
+
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    public Long getUserAnswerSeq() {
+        return userAnswerSeq;
+    }
+
+    public void setUserAnswerSeq(Long userAnswerSeq) {
+        this.userAnswerSeq = userAnswerSeq;
+    }
+
+    public short getOptionSeq() {
+        return optionSeq;
+    }
+
+    public void setOptionSeq(short optionSeq) {
+        this.optionSeq = optionSeq;
+    }
+    
+    
+}

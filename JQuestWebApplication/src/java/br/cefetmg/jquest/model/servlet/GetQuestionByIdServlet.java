d339dfedddf13c6c520f82f191a2c8a49134edc5
paulamararibeiro <paula.mribeiro05@gmail.com>
2017-07-16 16:46:20 -0300

    Updates servlet GetQuestionById

--- JQuestWebApplication/src/java/br/cefetmg/jquest/model/servlet/GetQuestionByIdServlet.java
+++ JQuestWebApplication/src/java/br/cefetmg/jquest/model/servlet/GetQuestionByIdServlet.java
@@ -5,11 +5,17 @@
  */
 package br.cefetmg.jquest.model.servlet;
 
+import br.cefetmg.jquest.model.dao.DomainDAOImpl;
+import br.cefetmg.jquest.model.dao.ModuleDAOImpl;
 import br.cefetmg.jquest.model.dao.QuestionAlternativeDAOImpl;
 import br.cefetmg.jquest.model.dao.QuestionDAOImpl;
 import br.cefetmg.jquest.model.domain.Question;
 import br.cefetmg.jquest.model.domain.QuestionAlternative;
 import br.cefetmg.jquest.model.exception.PersistenceException;
+import br.cefetmg.jquest.model.service.DomainManagement;
+import br.cefetmg.jquest.model.service.DomainManagementImpl;
+import br.cefetmg.jquest.model.service.ModuleManagement;
+import br.cefetmg.jquest.model.service.ModuleManagementImpl;
 import br.cefetmg.jquest.model.service.QuestionAlternativeManagement;
 import br.cefetmg.jquest.model.service.QuestionAlternativeManagementImpl;
 import br.cefetmg.jquest.model.service.QuestionManagement;
@@ -17,6 +23,8 @@ import br.cefetmg.jquest.model.service.QuestionManagementImpl;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,6 +38,8 @@ import javax.servlet.http.HttpServletResponse;
 public class GetQuestionByIdServlet extends HttpServlet {
     private Question question;
     private QuestionManagement questionManagement;
+    private DomainManagement domainManagement;
+    private ModuleManagement moduleManagement;
     private QuestionAlternativeManagement questionAlternativeManagement;
     private String result;
     
@@ -59,26 +69,22 @@ public class GetQuestionByIdServlet extends HttpServlet {
             questionManagement = new QuestionManagementImpl(QuestionDAOImpl.getInstance());
             question = questionManagement.getQuestionById(id);   
             
-            
-            //busca alternativas no banco de dados
-            questionAlternativeManagement = new QuestionAlternativeManagementImpl(QuestionAlternativeDAOImpl.getInstance());
-            List<QuestionAlternative> alternativesList = questionAlternativeManagement.getAlternativesByQuestionId(id);
-            
             //testa se questão foi recebida
             if (question != null) {
-                result = "{"
-                         + "\"id\": " + question.getId()
-                         + ", \"heading\": \"" + question.getHeadline()
-                         + "\" ,\"isMultipleChoice\": " + this.isMultipleChoice(question.getType()) 
-                         + ", \"correct\": " + this.selectCorrectAnswer(alternativesList)
-                         + ", \"alternatives\": [";
-                                for (QuestionAlternative alternative: alternativesList) {
-                                    result += "\"" + alternative.getAssertionText();
-                                    result += "\", ";
-                                }
-                int ult = result.lastIndexOf(',');
-                result = result.substring(0, ult);
-                result += "]}";
+                result = "";
+                switch(question.getType()) {
+                    case 'A':
+                        result += this.dissertiveQuestion(question);
+                        break;
+
+                    case 'M':
+                        result += this.multipleChoiceQuestion(question);
+                        break;
+
+                    case 'V':
+                        result += this.trueOrFalseQuestion(question);
+                        break;
+                }
 
             }
 
@@ -107,8 +113,68 @@ public class GetQuestionByIdServlet extends HttpServlet {
         return "Gets a question by its Id";
     }// </editor-fold>
     
-    private boolean isMultipleChoice(char idt) {
-        return idt == 'M';
+    private String multipleChoiceQuestion(Question q) {
+        List<QuestionAlternative> alternativesList = getAlternatives(q.getId());
+        String res = "{"
+                + "\"id\": " + q.getId()
+                + ", \"domain\": \"" + this.getDomain(q.getDomainId()) + "\""
+                + ", \"module\": " + this.getModule(q.getModuleId(), q.getDomainId()) + "\""
+                + ", \"heading\": \"" + q.getHeadline() + "\""
+                + ", \"idt\": \"" + q.getType() + "\""
+                + ", \"correctIndex\": " + this.selectCorrectAnswer(alternativesList)
+                + ", \"alternatives\": [";
+                       for (QuestionAlternative alternative: alternativesList) {
+                           res += "\"" + alternative.getAssertionText()
+                               + "\", ";
+                       }
+       int ult = res.lastIndexOf(',');
+       res = res.substring(0, ult);
+       res += "]}";
+       return res;
+    }
+    
+    private String trueOrFalseQuestion (Question q) {
+        List<QuestionAlternative> alternativesList = getAlternatives(q.getId());
+        String res = "{"
+                + "\"id\": " + q.getId()
+                + ", \"domain\": \"" + this.getDomain(q.getDomainId()) + "\""
+                + ", \"module\": " + this.getModule(q.getModuleId(), q.getDomainId()) + "\""
+                + ", \"heading\": \"" + q.getHeadline() + "\""
+                + ", \"idt\": \"" + q.getType() + "\"" 
+                + ", \"alternatives\": [";
+                       for (QuestionAlternative alternative: alternativesList) {
+                           res += "{"
+                               + "\"assertive\": \"" + alternative.getAssertionText() + "\""
+                               + ", \"isCorrect\": " + alternative.isIsCorrect()
+                               + "},    ";
+                       }
+       int ult = res.lastIndexOf(',');
+       res = res.substring(0, ult);
+       res += "]}";
+       return res;
+    }
+    
+    private String dissertiveQuestion (Question q) {
+        String res = "{"
+                + "\"id\": " + q.getId()
+                + ", \"domain\": \"" + this.getDomain(q.getDomainId()) + "\""
+                + ", \"module\": " + this.getModule(q.getModuleId(), q.getDomainId()) + "\""
+                + ", \"heading\": \"" + q.getHeadline() + "\""
+                + ", \"idt\": \"" + q.getType() + "\""
+                + "}";
+        return res;
+    }
+    
+    private List<QuestionAlternative> getAlternatives(Long questionId) {
+        List <QuestionAlternative> list = null;
+        try {
+            //busca alternativas no banco de dados
+            questionAlternativeManagement = new QuestionAlternativeManagementImpl(QuestionAlternativeDAOImpl.getInstance());
+            list = questionAlternativeManagement.getAlternativesByQuestionId(questionId);              
+        } catch (PersistenceException ex) {
+            Logger.getLogger(GetQuestionsServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
     }
     
     private Long selectCorrectAnswer (List<QuestionAlternative> alternatives) {
@@ -118,6 +184,28 @@ public class GetQuestionByIdServlet extends HttpServlet {
                 seq = alternative.getOptionSeq();
         }
         return seq;
+    }
+    
+    private String getDomain(Long domainId) {
+        String domain = "";
+        try {
+            domainManagement = new DomainManagementImpl(DomainDAOImpl.getInstance());
+            domain = domainManagement.getDomainById(domainId).getName();
+        } catch (PersistenceException ex) {
+            Logger.getLogger(GetQuestionsServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return domain;
+    }
+    
+    private String getModule(Long moduleId, Long domainId) {
+        String module = "";
+        try {
+            moduleManagement = new ModuleManagementImpl(ModuleDAOImpl.getInstance());
+            module = moduleManagement.getModuleById(moduleId, domainId).getName();
+        } catch (PersistenceException ex) {
+            Logger.getLogger(GetQuestionsServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return module;
     }
     
 }
